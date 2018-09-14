b838568b970f328a6834843038d1f3840479a494
YVEN SHANE <yvenshane@aol.com>
2018-09-14 15:38:07 +0800

    Finish tabBar controller

---
+++ EDAWCulture/EDAWCulture/Classes/HomePage/Controller/VENHomePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  VENHomePageViewController.h
+//  EDAWCulture
+//
+//  Created by YVEN on 2018/9/14.
+//  Copyright © 2018年 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VENHomePageViewController : UIViewController
+
+@end
---
+++ EDAWCulture/EDAWCulture/Classes/HomePage/Controller/VENHomePageViewController.m
@@ -0,0 +1,37 @@
+//
+//  VENHomePageViewController.m
+//  EDAWCulture
+//
+//  Created by YVEN on 2018/9/14.
+//  Copyright © 2018年 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import "VENHomePageViewController.h"
+
+@interface VENHomePageViewController ()
+
+@end
+
+@implementation VENHomePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
